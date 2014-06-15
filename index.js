74cccdcde919458dced92925a134025d6e9079ac
jonschlinkert <dev@sellside.com>
2014-06-15 03:58:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/*!
+ * array-sum <https://github.com/jonschlinkert/array-sum>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT License
+ */
+
+'use strict';
+
+module.exports = function arraySum(arr) {
+  return arr.reduce(function(a, b, i, c) {
+    a = typeof a === 'number' ? a : 0;
+    b = typeof b === 'number' ? b : 0;
+    return a + b;
+  });
+};
